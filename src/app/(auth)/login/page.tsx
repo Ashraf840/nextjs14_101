7d893fec7af725190692b0c4fd20847937e29048
Ashraf840 <shift2uleashed2011@gmail.com>
2024-04-05 22:19:10 +0600

    Route groups: logical grouping of routes & project files yet leaving the URL file structure un-altered

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// NB: Route Groups; Wrap the parent-dir with parentheses
+// Logically group the routes ans project files without affecting the the URL path structure. Thus we don't need to mention "/auth/..." in the URL path.
+export default function Login() {
+    return (
+        <div>Login Page</div>
+    )
+}
