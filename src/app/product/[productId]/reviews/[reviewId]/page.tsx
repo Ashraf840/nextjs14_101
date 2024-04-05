1b66cea3017f7c180f3c81ccda073e25534b9bdf
Ashraf840 <shift2uleashed2011@gmail.com>
2024-04-05 16:29:15 +0600

    Programmatically render the not-found page based on certain condition

--- src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { notFound } from 'next/navigation';
 
 type ReviewDetailProps = {
     params: {
@@ -8,6 +9,11 @@ type ReviewDetailProps = {
 }
 
 export default function ReviewDetail({ params }: ReviewDetailProps) {
+    // This logic can be applied in the searching mechanism
+    if (parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
+
     return (
         <>
             <h1>Review Detail</h1>
